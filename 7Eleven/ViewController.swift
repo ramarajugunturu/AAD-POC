d8b062e84d8a95196a18c5dd787d4aea2c11ba2a
ramaraju_gunturu <ramaraju.gunturu@infovisionlabs.com>
2018-06-01 08:18:35 +0530

    Initial Commit

---
+++ 7Eleven/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  7Eleven
+//
+//  Created by Rama Raju Gunturu on 01/06/18.
+//  Copyright © 2018 InfoVision Labs India Pvt. Ltd. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
